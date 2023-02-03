ad684323d6f4c930a48b7086902157a52ac95b90
rakeshgowda7 <as.rakeshgowda@gmail.com>
2023-02-03 16:09:15 +0530

    1st test

--- src/test/java/tests/TestExamples.java
+++ src/test/java/tests/TestExamples.java
@@ -1,7 +1,33 @@
 package tests;
 
-public class TestExamples {
-	
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class TestExamples {
+
+	@Test(enabled = false)
+	public void test_1() {
+		Response response = get("https://reqres.in/api/users?page=2");
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+
+	}
+
+	@Test
+	public void test_2() {
+//		baseURI = "https://reqres.in/api/";
+		given().get("https://reqres.in/api/users?page=2").then().statusCode(200).body("data[0].id", equalTo(7)).log().all();
+
+	}
 
 }
